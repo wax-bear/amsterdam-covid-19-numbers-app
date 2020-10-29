47019f1ab7ff0393416f75960a851b739cf0deac
Malcolm Kente <malcolm.kente@remrkabledev.com>
2020-10-29 12:54:20 +0100

    Introduced services folder for api services

--- controllers/index.js
+++ controllers/index.js
@@ -2,13 +2,12 @@ const fetch = require("node-fetch");
 
 const { getDailyC19AmsData } = require("../helpers");
 
+const { fetchCovidJSONData } = require("../services/");
+
 exports.getDailyAmsDataController = async (_, response) => {
   try {
-    const res = await fetch(
-      "https://data.rivm.nl/covid-19/COVID-19_aantallen_gemeente_per_dag.json"
-    );
+    const res = await fetchCovidJSONData();
     const dailyC19AmsData = await getDailyC19AmsData(res);
-
     response.render("index", {
       title: "People tested positive on COVID-19 in Amsterdam today",
       dailyC19AmsData,
---
+++ services/index.js
@@ -0,0 +1,12 @@
+const fetch = require("node-fetch");
+
+exports.fetchCovidJSONData = async () => {
+  try {
+    return await fetch(
+      "https://data.rivm.nl/covid-19/COVID-19_aantallen_gemeente_per_dag.json"
+    );
+  } catch (error) {
+    const errorObjString = JSON.stringify(err, Object.getOwnPropertyNames(err));
+    console.error("Covid API request failed, error object: " + errorObjString);
+  }
+};
