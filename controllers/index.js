00bc415022b1eb9f9bf5cc4d9532cbafacf2038a
Malcolm Kente <malcolm.kente@remrkabledev.com>
2020-10-29 12:39:05 +0100

    Introduced controllers folder and refactored route

---
+++ controllers/index.js
@@ -0,0 +1,20 @@
+const fetch = require("node-fetch");
+
+const { getDailyC19AmsData } = require("../helpers");
+
+exports.getDailyAmsDataController = async (request, response) => {
+  try {
+    const res = await fetch(
+      "https://data.rivm.nl/covid-19/COVID-19_aantallen_gemeente_per_dag.json"
+    );
+    const dailyC19AmsData = await getDailyC19AmsData(res);
+
+    response.render("index", {
+      title: "People tested positive on COVID-19 in Amsterdam today",
+      dailyC19AmsData,
+    });
+  } catch (err) {
+    const errorObjString = JSON.stringify(err, Object.getOwnPropertyNames(err));
+    console.error("Request failed, error object: " + errorObjString);
+  }
+};
--- routes/index.js
+++ routes/index.js
@@ -1,22 +1,9 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const fetch = require('node-fetch');
+const fetch = require("node-fetch");
 
-const getDailyC19AmsData = require('../helpers').getDailyC19AmsData;
+const { getDailyAmsDataController } = require("../controllers");
 
-router.get('/', async (request, response) => {
-  try {
-    const res = await fetch('https://data.rivm.nl/covid-19/COVID-19_aantallen_gemeente_per_dag.json');
-    const dailyC19AmsData = await getDailyC19AmsData(res);
-
-    response.render('index', {
-      title: 'People tested positive on COVID-19 in Amsterdam today',
-      dailyC19AmsData
-    });
-  } catch (err) {
-    const errorObjString = JSON.stringify(err, Object.getOwnPropertyNames(err));
-    console.error('Request failed, error object: ' + errorObjString);
-  }
-});
+router.get("/", getDailyAmsDataController);
 
 module.exports = router;
