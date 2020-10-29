85bef844bad91c2f5cb1eb7cf6f96bcad698516a
Lloyd Chambrier <lloydchambrier@gmail.com>
2020-10-29 22:57:03 +0100

    Switched engine to handlebars && Reversed dates rendering order && Added some styling

--- controllers/index.js
+++ controllers/index.js
@@ -6,7 +6,7 @@ exports.getDailyAmsDataController = async (_, response) => {
     const res = await fetchCovidJSONData();
     const dailyC19AmsData = await getDailyC19AmsData(res);
     response.render("index", {
-      title: "People tested positive on COVID-19 in Amsterdam today",
+      today: `Number of infected people today: ${dailyC19AmsData[0].Total_reported} people`,
       dailyC19AmsData,
     });
   } catch (err) {
