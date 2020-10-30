85c1ce9fe90388c4bef7800deb12497e153d6c40
Lloyd Chambrier <lloydchambrier@gmail.com>
2020-10-30 12:39:02 +0100

    Removed style attribute && updated error.hbs

--- app.js
+++ app.js
@@ -33,7 +33,7 @@ app.use(function (err, req, res) {
 
   // render the error page
   res.status(err.status || 500);
-  res.render("error");
+  res.render("error", {err});
 });
 
 module.exports = app;
