db9f970649cffd34dae234a70bd5089eb1d5cb16
Thomas Wassenberg <tm.wassenberg@gmail.com>
2020-11-01 10:44:38 +0100

    Remove trailing comma
    Remove trailing comma from getDailyAmsDataController import

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { getDailyAmsDataController, } = require('../controllers');
+const { getDailyAmsDataController } = require('../controllers');
 const router = express.Router();
 
 router.get('/', getDailyAmsDataController);
